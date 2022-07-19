4c6c00b90f5a59b33b7e17436b97b8f0dc35c55d
faidrn <m3talmaniaco@gmail.com>
2022-07-19 17:10:53 -0500

    Add functionalities of the lateral menu with js

--- assets/js/functionalities.js
+++ assets/js/functionalities.js
@@ -36,3 +36,72 @@ function clarInput(){
 	//mostar icono buscar
 	showIconSearch();
 }
+
+let flag = 0;
+function lateralMenu(){
+	const menu = document.querySelectorAll('.lateral-menu-bar');
+	const optionMenuToHide = document.querySelectorAll('.visibility-option-menu');
+	const optionMenuToShow = document.querySelectorAll('.not-visibility-option-menu');
+	const borderBottomFirstSection = document.querySelectorAll('.first-section');
+	const lateralMenu = document.querySelectorAll('.lateral-menu');
+	const flexDirectionRow = document.querySelectorAll('.flex-direction-row');
+	const flexDirectionColumn = document.querySelectorAll('.flex-direction-column');
+	
+	if (flag == 0){
+		// Cambiar el ancho del menu (reducir)
+		menu[0].classList.remove('lateral-menu-expanded');
+		menu[0].classList.add('lateral-menu-not-expanded');
+		
+		// Ocutar opciones
+		//recorrer TODOS los menus
+		optionMenuToHide.forEach((everyOption, i) => {
+			//Removiendo y asignando clases
+			optionMenuToHide[i].classList.remove('visibility-option-menu')
+			optionMenuToHide[i].classList.add('not-visibility-option-menu')
+		});
+		
+		//quitar la primer division
+		borderBottomFirstSection[0].style.borderBottom = 'none';
+		
+		// Quitar el scroll
+		menu[0].classList.remove('scroll-y');
+		
+		//Cambiar de row a column el flexbox
+		flexDirectionRow.forEach((everyOption, i) => {
+			//Removiendo y asignando clases
+			flexDirectionRow[i].classList.remove('flex-direction-row')
+			flexDirectionRow[i].classList.add('flex-direction-column')
+		});
+		
+		flag += 1;
+	} else{
+		// Cambiar el ancho del menu (Ampliar)
+		menu[0].classList.remove('lateral-menu-not-expanded');
+		menu[0].classList.add('lateral-menu-expanded');
+		
+		// Mostrar opciones
+		//menuToHide[0].classList.remove('not-visibility-option-menu');
+		//menuToHide[0].classList.add('visibility-option-menu');
+		//recorrer TODOS los menus
+		optionMenuToShow.forEach((everyOption, i) => {
+			//Removiendo y asignando clases
+			optionMenuToShow[i].classList.remove('not-visibility-option-menu')
+			optionMenuToShow[i].classList.add('visibility-option-menu')
+		});
+		
+		//agregar la primer division
+		borderBottomFirstSection[0].style.borderBottom = '1px solid var(--borders-color)';
+		
+		//Aggregar el scroll
+		menu[0].classList.add('scroll-y');
+		
+		//Cambiar de column a row el flexbox
+		flexDirectionColumn.forEach((everyOption, i) => {
+			//Removiendo y asignando clases
+			flexDirectionColumn[i].classList.remove('flex-direction-column')
+			flexDirectionColumn[i].classList.add('flex-direction-row')
+		});
+		flag -= 1;
+	}
+	
+}
